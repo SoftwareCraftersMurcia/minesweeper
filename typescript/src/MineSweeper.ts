2ad268cdef6725259123dd078f2d205caff6901a
Luis Rovirosa Mairlot <luis@codium.team>
2025-04-25 15:50:54 +0200

    a field with no mines has all the solution as zeros

--- typescript/src/MineSweeper.ts
+++ typescript/src/MineSweeper.ts
@@ -1,5 +1,7 @@
 export class MineSweeper {
   solve (xSize: number, ySize: number, field: string[][]): string[][] {
-    return [];
+      return Array(ySize)
+          .fill(null)
+          .map(() => Array(xSize).fill('0'));
   }
 }
--- typescript/tests/MineSweeper.spec.ts
+++ typescript/tests/MineSweeper.spec.ts
@@ -1,20 +1,37 @@
 import { MineSweeper } from '../src/MineSweeper'
 
 describe('MineSweeper', () => {
-  it.skip('acceptance test', () => {
-    const mineSweeper = new MineSweeper()
+    it('a field with no mines has all the solution as zeros', () => {
+        const mineSweeper = new MineSweeper()
 
-      let field = [
-          ['*', '.', '.', '.'],
-          ['.', '.', '*', '.'],
-          ['.', '.', '.', '.'],
-      ];
-      let solution = mineSweeper.solve(4, 3, field);
+        let field = [
+            ['.', '.', '.', '.'],
+            ['.', '.', '.', '.'],
+            ['.', '.', '.', '.'],
+        ];
+        let solution = mineSweeper.solve(4, 3, field);
 
-      expect(solution).toBe([
-          ['*', '2', '1', '1'],
-          ['1', '2', '*', '1'],
-          ['0', '1', '1', '1'],
-      ]);
-  });
+        expect(solution).toEqual([
+            ['0', '0', '0', '0'],
+            ['0', '0', '0', '0'],
+            ['0', '0', '0', '0'],
+        ]);
+    });
+
+    it.skip('acceptance test', () => {
+        const mineSweeper = new MineSweeper()
+
+        let field = [
+            ['*', '.', '.', '.'],
+            ['.', '.', '*', '.'],
+            ['.', '.', '.', '.'],
+        ];
+        let solution = mineSweeper.solve(4, 3, field);
+
+        expect(solution).toBe([
+            ['*', '2', '1', '1'],
+            ['1', '2', '*', '1'],
+            ['0', '1', '1', '1'],
+        ]);
+    });
 })
