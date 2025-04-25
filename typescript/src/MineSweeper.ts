9ed041fb53993d5092b9eb95706e81b7fe7e5f79
Luis Rovirosa Mairlot <luis@codium.team>
2025-04-25 16:01:47 +0200

    refactor to be able to control the flow

--- typescript/src/MineSweeper.ts
+++ typescript/src/MineSweeper.ts
@@ -1,7 +1,12 @@
 export class MineSweeper {
-  solve (xSize: number, ySize: number, field: string[][]): string[][] {
-      return Array(ySize)
-          .fill(null)
-          .map(() => Array(xSize).fill('0'));
-  }
+    solve(xSize: number, ySize: number, field: string[][]): string[][] {
+        const result: string[][] = []
+        for (let i = 0; i < ySize; i++) {
+            result.push([]);
+            for (let j = 0; j < xSize; j++) {
+                result[i].push('0');
+            }
+        }
+        return result;
+    }
 }
