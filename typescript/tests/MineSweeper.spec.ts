2fb444544c27bf9f99b021637b1fee2a3371d84d
Luis Rovirosa Mairlot <luis@codium.team>
2025-04-25 15:35:32 +0200

    rename classes

---
+++ typescript/tests/MineSweeper.spec.ts
@@ -0,0 +1,9 @@
+import { MineSweeper } from '../src/MineSweeper'
+
+describe('MineSweeper', () => {
+  it('example test', () => {
+    const mineSweeper = new MineSweeper()
+
+    expect(mineSweeper.method()).toBe(true)
+  })
+})
