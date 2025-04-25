32653cbddefc1be23b6404f3070510dad22a7c48
Carlos Castillo <castillo.st@gmail.com>
2025-04-25 18:19:46 +0200

    inital commit

--- typescript/tests/Example.spec.ts
+++ typescript/tests/Example.spec.ts
@@ -1,9 +1,33 @@
 import { Example } from '../src/Example'
 
 describe('kata-setup', () => {
+  
+  let mat: string[][];
+  beforeEach(() => {
+    mat = [
+      ['.', '.', '*'],
+      ['.', '*', '.'],
+      ['.', '.', '.']
+    ]
+  })
   it('example test', () => {
     const example = new Example()
 
     expect(example.method()).toBe(true)
   })
 })
+/*
+
+* . . .
+. . * .
+. . . .
+
+*/
+
+/*
+
+* 2 1 1
+1 2 * 1
+0 1 1 1
+
+*/
