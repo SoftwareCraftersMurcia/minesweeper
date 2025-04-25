e80a0722891b8ac4f4409db2cca55f13491ce040
Luis Rovirosa Mairlot <luis@codium.team>
2025-04-25 15:54:42 +0200

    add tests skipped: a field with 1 mine in the center has all adjacent to 1

--- typescript/tests/MineSweeper.spec.ts
+++ typescript/tests/MineSweeper.spec.ts
@@ -18,6 +18,23 @@ describe('MineSweeper', () => {
         ]);
     });
 
+    it.skip('a field with 1 mine in the center has all adjacent to 1', () => {
+        const mineSweeper = new MineSweeper()
+
+        let field = [
+            ['.', '.', '.', '.'],
+            ['.', '.', '*', '.'],
+            ['.', '.', '.', '.'],
+        ];
+        let solution = mineSweeper.solve(4, 3, field);
+
+        expect(solution).toEqual([
+            ['0', '1', '1', '1'],
+            ['0', '1', '*', '1'],
+            ['0', '1', '1', '1'],
+        ]);
+    });
+
     it.skip('acceptance test', () => {
         const mineSweeper = new MineSweeper()
 
